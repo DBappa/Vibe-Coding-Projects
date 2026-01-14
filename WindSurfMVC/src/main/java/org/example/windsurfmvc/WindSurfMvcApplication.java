f15727aeb51e767b66f853c8caa0ac20894674c5
DBappa <bappadityadutta2024@gmail.com>
2026-01-14 23:55:34 +0530

    Creating Spring Boot Template for Vibe Coding

---
+++ WindSurfMVC/src/main/java/org/example/windsurfmvc/WindSurfMvcApplication.java
@@ -0,0 +1,13 @@
+package org.example.windsurfmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WindSurfMvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WindSurfMvcApplication.class, args);
+    }
+
+}
---
+++ WindSurfMVC/src/test/java/org/example/windsurfmvc/WindSurfMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.windsurfmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WindSurfMvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
